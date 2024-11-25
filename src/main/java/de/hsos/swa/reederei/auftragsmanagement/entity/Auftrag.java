ba75f9fd96297e2f0019b61def666c1ef0c68e95
dabregen <daniel.bregen@hs-osnabrueck.de>
2024-11-25 21:34:59 +0100

    jpa entity versucht nach student beispiel zu bauen

--- src/main/java/de/hsos/swa/reederei/auftragsmanagement/entity/Auftrag.java
+++ src/main/java/de/hsos/swa/reederei/auftragsmanagement/entity/Auftrag.java
@@ -1,4 +1,52 @@
 package de.hsos.swa.reederei.auftragsmanagement.entity;
 
+import java.util.Date;
+
 public class Auftrag {
+    private long id;
+    private String beschreibung;
+    private Date eingangsdatum;
+    private String schiffURL;
+
+    public Auftrag() {
+    }
+
+    public Auftrag(long id, String beschreibung, Date eingangsdatum, String schiffURL) {
+        this.id = id;
+        this.beschreibung = beschreibung;
+        this.eingangsdatum = eingangsdatum;
+        this.schiffURL = schiffURL;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getBeschreibung() {
+        return beschreibung;
+    }
+
+    public void setBeschreibung(String beschreibung) {
+        this.beschreibung = beschreibung;
+    }
+
+    public Date getEingangsdatum() {
+        return eingangsdatum;
+    }
+
+    public void setEingangsdatum(Date eingangsdatum) {
+        this.eingangsdatum = eingangsdatum;
+    }
+
+    public String getSchiffURL() {
+        return schiffURL;
+    }
+
+    public void setSchiffURL(String schiffURL) {
+        this.schiffURL = schiffURL;
+    }
 }
--- src/main/java/de/hsos/swa/reederei/auftragsmanagement/gateway/dto/AuftragJPAEntity.java
+++ src/main/java/de/hsos/swa/reederei/auftragsmanagement/gateway/dto/AuftragJPAEntity.java
@@ -1,4 +1,114 @@
 package de.hsos.swa.reederei.auftragsmanagement.gateway.dto;
 
-public class AuftragJPAEntity {
+import java.io.Serializable;
+import java.util.Date;
+
+import jakarta.persistence.Cacheable;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.Table;
+import jakarta.persistence.TableGenerator;
+import jakarta.persistence.Version;
+
+@Entity(name="Auftrag")
+@Table(name="AUFTRAEGE")
+@NamedQuery(name = "AuftragJPAEntity.findByBeschreibung",
+        query = "SELECT a FROM Auftrag a WHERE a.beschreibung=:beschreibung")
+@Cacheable(true)
+public class AuftragJPAEntity implements Serializable {
+    @Id
+    @TableGenerator(name="auftrag_generator", initialValue = 1)
+    @GeneratedValue(strategy = GenerationType.TABLE, generator = "auftrag_generator")
+    private Long id;
+
+    @Column(name="beschreibung")
+    private String beschreibung;
+
+    @Column(name="eingangsdatum")
+    private Date eingangsdatum;
+
+    @Column(name="schiff_url")
+    private String schiffURL;
+
+    @Version
+    private Long versionsnr;
+
+    public AuftragJPAEntity() {
+    }
+
+    public AuftragJPAEntity(String beschreibung, Date eingangsdatum, String schiffURL) {
+        this.beschreibung = beschreibung;
+        this.eingangsdatum = eingangsdatum;
+        this.schiffURL = schiffURL;
+    }
+
+    public AuftragJPAEntity(Long id, String beschreibung, Date eingangsdatum, String schiffURL) {
+        this.id = id;
+        this.beschreibung = beschreibung;
+        this.eingangsdatum = eingangsdatum;
+        this.schiffURL = schiffURL;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBeschreibung() {
+        return beschreibung;
+    }
+
+    public void setBeschreibung(String beschreibung) {
+        this.beschreibung = beschreibung;
+    }
+
+    public Date getEingangsdatum() {
+        return eingangsdatum;
+    }
+
+    public void setEingangsdatum(Date eingangsdatum) {
+        this.eingangsdatum = eingangsdatum;
+    }
+
+    public String getSchiffURL() {
+        return schiffURL;
+    }
+
+    public void setSchiffURL(String schiffURL) {
+        this.schiffURL = schiffURL;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        AuftragJPAEntity other = (AuftragJPAEntity) obj;
+        if (id == null) {
+            return other.id == null;
+        } else return id.equals(other.id);
+    }
+
+    @Override
+    public String toString() {
+        return "id=" + id + ", beschreibung=" + beschreibung + ", eingangsdatum=" + eingangsdatum + ", schiffURL=" + schiffURL;
+    }
 }
